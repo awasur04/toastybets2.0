e35cf14a0b34277ce0fa23f805fb8ab67c54600d
awasur04 <awasur04@yahoo.com>
2022-01-17 22:13:43 -0600

    added event registration

--- src/toastybets.js
+++ src/toastybets.js
@@ -1,5 +1,11 @@
 const config = require('./config.json');
 const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+/**
+ * Define the intents that are going to be used by the bot
+ * Reference: https://discord.com/developers/docs/topics/gateway
+ */
 const intents = new Intents();
 intents.add(
 	"GUILDS",
@@ -20,5 +26,28 @@ const client = new Client({ intents: intents });
 function initialize()
 {
 	client.login(config.token);
+	registerEvents();
 }
+
+/**
+ * registerEvents: Register all event files located in src/events folder
+ */
+function registerEvents()
+{
+	const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+	for (const file of eventFiles)
+	{
+		const event = require(`./events/${file}`);
+		if (event.once)
+		{
+			client.once(event.name, (...args) => event.execute(...args));
+		}
+		else
+		{
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+}
+
 initialize();
