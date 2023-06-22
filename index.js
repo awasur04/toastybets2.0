fa99836dab2734344cd59bef1ebf7fb595429054
awasur04 <awasur04@yahoo.com>
2023-06-22 18:42:13 -0500

    moved events to seperate files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 // Require the necessary discord.js classes
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -21,8 +21,9 @@ for (const folder of commandFolders)
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 	// For each command file inside the current sub folder
-	for (const file in commandFiles)
+	for (const file of commandFiles)
 	{
+		console.log(file);
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 
@@ -38,44 +39,24 @@ for (const folder of commandFolders)
 	}
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c =>
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles)
 {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once)
+	{
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else
+	{
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 
 // Log in to Discord with your client's token
 client.login(token);
-
-// Listener to interact with our commands
-client.on(Events.InteractionCreate, async interaction =>
-{
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandsName);
-
-	if (!command)
-	{
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try
-	{
-		await command.execute(interaction);
-	}
-	catch (error)
-	{
-		console.error(error);
-		if (interaction.replied || interaction.deferred)
-		{
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else
-		{
-			await interaction.reply({ content: 'There was an error while executing this command!', ephermal: true });
-		}
-	}
-	console.log(interaction);
-});
