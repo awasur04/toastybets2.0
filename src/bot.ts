8fe9ab96c2e020f23739f6ed7a11f7838be2a6bd
awasur04 <awasur04@yahoo.com>
2023-11-12 11:30:27 -0600

    Added database connection

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import { Client, GatewayIntentBits, Collection } from "discord.js";
-import { config } from "./config"
+import { config } from "./config";
+import { mysql } from "./utilities/database/query";
 
 //Event listeners
 import ready from "./events/ready";
@@ -9,10 +10,21 @@ import guildCreate from "./events/guildCreate";
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-//Define all the events
+// Define all the events
 ready(client);
 interactionCreate(client);
 guildCreate(client);
+
+
+// Connect to database
+mysql.query("SELECT VERSION()", []).then((status) => {
+	if (status === undefined)
+	{
+		throw new Error("Database failed to connect. (Verify the database is online)");
+	}
+
+	console.log("Database: connection established!");
+});
 
 
 // Log in to Discord with your client's token
---
+++ src/utilities/database/query.ts
@@ -0,0 +1,54 @@
+const mysql2 = require('mysql2/promise');
+import { config } from "../../config"
+
+export type GameResult = {
+	game_id: string;
+    home_team_id: number;
+    away_team_id: number;
+    home_score: number;
+    away_score: number;
+    game_status: string;
+    game_time: string;
+    home_odds: number;
+    away_odds: number;
+    week_number: number;
+};
+
+let queries = {
+	testConnection: "SELECT VERSION()",
+	// SELECT QUERIES
+	getAllGames: "SELECT * FROM scores",
+	getCurrentWeek: "SELECT * FROM scores WHERE week_number=?",
+	getAnyUserById: "SELECT * FROM users WHERE account_id=?",
+	getGuildUserById: "SELECT * FROM users WHERE account_id=? AND guild_id=?",
+	//INSERT QUERIES
+	insertNewUser: "INSERT INTO users (discord_id, guild_id, name, timezone, role, balance, status) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	//UPDATE QUERIES
+}
+
+let pool = mysql2.createPool(
+	{
+		connectionLimit: 5,
+		host: config.HOST,
+		user: config.USERNAME,
+		password: config.PASSWORD,
+		database: config.DATABASE
+	}
+);
+
+async function execute<TQueryResult = any>(query: string, params: any[]): Promise<TQueryResult | undefined> {
+	try {
+		const [ rows ] = await pool.query(query, params)
+		return rows;
+	}
+	catch(error: any) {
+		console.log(`MYSQL(execute): ${query} : ${params.join(', ')} failed to execute with error: ${error.message}`);
+		return undefined;
+	}
+}
+
+export const mysql = {
+	queries: queries,
+	query: execute
+};
+
